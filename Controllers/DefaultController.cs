9f77931dff6e100faca0d893873613ddde5844b7
Zsófi Gagyi <gagyizsofi@gmail.com>
2019-10-28 17:19:12 +0100

    first commit: create app, image, env variable

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace helloDocker.Controllers
+{
+    public class DefaultController : Controller
+    {
+        static int visitorCount = 0;
+
+        [HttpGet("/")]
+        public IActionResult Default()
+        {
+            visitorCount++;
+            ViewData["visitorNumber"] = visitorCount;
+            ViewData["enviromentalFlower"] = System.Environment.GetEnvironmentVariable("flower");
+            return View();
+        }
+    }
+}
