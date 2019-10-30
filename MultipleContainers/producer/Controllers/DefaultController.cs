e44aa5d98f379989f0f547979ed0accd51691005
Zsófi Gagyi <gagyizsofi@gmail.com>
2019-10-30 14:02:45 +0100

    add: a producer app for a multi-container setup

---
+++ MultipleContainers/producer/Controllers/DefaultController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+
+namespace helloDocker.Controllers
+{
+    public class DefaultController : Controller
+    {
+        static int visitorCount = 0;
+        private readonly HttpClient _client;
+
+        public DefaultController(IHttpClientFactory httpClientFactory)
+        {
+            _client = httpClientFactory.CreateClient("consumer");
+        }
+
+        [HttpGet("/")]
+        public  IActionResult Default()
+        {
+            visitorCount++;
+            ViewData["visitorNumber"] = visitorCount;
+            ViewData["enviromentalFlower"] = System.Environment.GetEnvironmentVariable("flower");
+
+            NotifyConsumer();  
+
+            return View();
+        }
+
+        private void NotifyConsumer()
+        {
+            var news = new { Event = "Visitor nr." + visitorCount + "has arrived!" };
+            var newsJson = JsonConvert.SerializeObject(news);
+            HttpContent message = new StringContent(newsJson, Encoding.UTF8, "application/json");
+
+            _client.PostAsync("", message);
+        }
+    }
+}
